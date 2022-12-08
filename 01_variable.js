d22294ce8968a295dd73615850fe733b476b1781
kiranhelwadkar <kiranhelwadkar@gmail.com>
2022-12-08 09:38:48 +0530

    Java Code
    I have done first code in JAVA script_12 December 2022

---
+++ 01_variable.js
@@ -0,0 +1,10 @@
+var myName; //variable Declaration  
+myName = "Kiran Helwadkar"; //variable intialization
+console.log(myName);
+
+var myFrdName = "Gajanan Lande"; //variable declaration and intilization at the same time
+console.log(myFrdName);
+var myFrdName = "Ekanath Helwadkar";
+console.log(myFrdName);  
+
+
---
+++ 01_variable_assign.js
@@ -0,0 +1,19 @@
+var myFullName; 
+myFullName = "Full Name"
+console.log(myFullName)
+
+var myFullName; 
+myFullName = "Kiran Helwadkar"
+console.log(myFullName)
+
+var myFullName; 
+myFullName = "Kiran Bhikaji Helwadkar"
+console.log(myFullName)
+
+var myLove; 
+myLove = "I Love Only JS"
+console.log(myLove)
+
+var firstName, lastName, age, address, pin_code, gender, is_married;
+firstName = "Kiran", lastName = "Helwadkar", age = "25 Years", address = "Mumbai", pin_code = "400013", gender = "Male", is_married = "no"
+console.log(firstName,lastName,age, address, pin_code, gender, is_married)
