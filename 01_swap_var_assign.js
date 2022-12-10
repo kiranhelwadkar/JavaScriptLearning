117d0e06f25922ab31828778ad08e209e80ba5aa
kiranhelwadkar <kiranhelwadkar@gmail.com>
2022-12-10 06:48:07 +0530

    Created Swap JS file
    Created Swap JS file

---
+++ 01_swap_var_assign.js
@@ -0,0 +1,27 @@
+
+var sweety = "Sweety";
+var cutie = "Cutie";
+    console.log ("====Before Swap====");
+console.log(sweety, cutie );
+
+console.log ("====After Swap====");
+var temp = "sweety";
+var sweety = "cutie";
+var cutie = "temp";
+console.log(sweety, temp );
+
+var num1 = 100;
+var num2 = 200;
+var num3 = 300;
+console.log("====Before Swap====");
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log("====After Swap====");
+var temp = 100;
+var num1 = num2;
+var num2 = num3;
+var num3 = temp;
+console.log(num1);
+console.log(num2);
+console.log(temp);
