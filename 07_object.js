51caf963ca289426c9b3b3114fd2281700fa7518
kiranhelwadkar <kiranhelwadkar@gmail.com>
2022-12-28 18:28:50 +0530

    code done and submitted
    code done and submitted

--- 07_object.js
+++ 07_object.js
@@ -4,9 +4,9 @@ const person = {
     age: 23,
     name: "Billgates",
     speak: function(){
-        console.log(`Hey I can speak English and Hindi`);
-      }
-    
+      console.log(`Hey I can speak English and Hindi`);
+    }
+  
   }
   console.log(typeof person);
   console.log(person);
@@ -28,65 +28,67 @@ const person = {
   console.log(`=== Deleting an property into an object====`);
   delete person.weight;
   console.log(person);
-
+  
   console.log(`=== Accessing an function from an object====`);
-person.speak();
-
+  person.speak();
+  
   console.log(`=== Creating an empty object====`);
-const mobile = {
-
-}
-mobile.company = "Micromax";
-mobile.camera = "20Px";
-console.log(mobile);
-
-
-console.log(`Nested object`);
-const student = {
-    name: "Stew Jobs",
-    age: 50,
-    marks: {
-        science: 70,
-        math: 90,
-        english: 87,
-        add: function() {
-            familyMemberNames: [ 'Tony', 'Lonny', 'Jenny', 'Watigton']
-
-            return this.science+this.math+this.english;
-         }
- 
-    }
-}
-console.log(`Accessing an Nested object property`);
-const scienceMark =  student.marks.science;
-console.log(scienceMark);
-
-console.log(`Updating an Nested object property`);
-student.marks.english = 97;
-
-console.log(`Adding an Nested object property`);
-student.marks.programming = 100;
-
-console.log(`Deleting an Nested object property`);
-delete student.marks.english;
-
-console.log(`Accessing an nested oject propertyc`);
-
-console.log(`Accessing an array`);
-console.log(student.familyMemberNames);
-
-console.log(`Object entries`);
-const billgates = {
-    height: 6,
-    weight: 70,
-    age: 23,
-    name: "Billgates"
-}
-console.log(Object.entries(billgates));
-console.log(Object.keys(billgates));
-console.log(Object.values(billgates));
-
-
-
-
+  const mobile = {
+  
+  }
+  mobile.company = "Micromax";
+  mobile.camera = "20Px";
+  console.log(mobile);
+  
+  person.walk= function(){
+      console.log(`Hey I can walk and run man`);
+  }
+  console.log(person);
+  
+  console.log(`Nested object`);
+  const student = {
+      name: "Stew Jobs",
+      age: 50,
+      marks: {
+          science: 70, 
+          math: 90,
+          english: 87,
+          add: function() {
+             return this.science+this.math+this.english;
+          }
+      },
+      familyMemberNames: [ 'Tony', 'Lonny', 'Jenny', 'Watigton']
+  }
+  console.log(`Accessing an Nested object property`);
+  const scienceMark =  student.marks.science;
+  console.log(scienceMark);
+  
+  console.log(`Updating an Nested object property`);
+  student.marks.english = 97;
+  
+  console.log(`Adding an Nested object property`);
+  student.marks.programming = 100;
+  
+  console.log(`Deleting an Nested object property`);
+  // delete student.marks.english;
+  
+  console.log(`Accessing an Nested object function`);
+  const totalMarks = student.marks.add();
+  console.log(`Total Marks: ${totalMarks}`);
+  
+  console.log(`Accessing an array`);
+  console.log(student.familyMemberNames);
+  
+  console.log(`Object entries`);
+  const billgates = {
+      height: 6,
+      weight: 70,
+      age: 23,
+      name: "Billgates"
+  }
+  console.log(Object.entries(billgates));
+  console.log(Object.keys(billgates));
+  console.log(Object.values(billgates));
+  
+  
   
---
+++ 07_objectAssignment_02.js
@@ -0,0 +1,45 @@
+console.log(`SBI Bank Details`);
+
+const sbiBank = {
+    bankName : "SBI",
+    location: "Mumbai",
+    accountNo: 65653457565,
+    ifsc : "SBIN0070001",
+    interestRate : 5.8,
+}
+console.log(sbiBank);
+
+console.log(``);
+console.log(`Axis Bank Details`);
+
+const axixBank = {
+    bankName : "Axis",
+    location: "Pune",
+    accountNo: 44778333567,
+    ifsc : "UTIB0000037",
+    interestRate : 6.7,
+}
+console.log(axixBank);
+
+console.log(``);
+console.log(`HDFC Bank Details`);
+
+const hdfcBank = {
+    bankName : "HDFC",
+    location: "Kolhapur",
+    accountNo: 776435736844,
+    ifsc : "HDFC0000164",
+    interestRate : 6.4,
+}
+console.log(hdfcBank);
+
+console.log(``);
+console.log(`Yes Bank Details`);
+const yesBank = {
+    bankName : "Yes",
+    location: "Goa",
+    accountNo: 13554656767,
+    ifsc : "YESB0000027",
+    interestRate : 6.4,
+}
+console.log(yesBank);
