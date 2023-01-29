eacd23fcf52b0d3cd38bca58db9ec7f560b6e74d
kiranhelwadkar <kiranhelwadkar@gmail.com>
2023-01-29 08:01:54 +0530

    Update 04_Nan.js

--- 04_Nan.js
+++ 04_Nan.js
@@ -1,6 +1,6 @@
 var resultDivide = 0/0;
 console.log(`Result is: ${resultDivide}`);
-console.log(`Type of${resultDivide} is ${typeof resultDivide}`);
+console.log(`Type of ${resultDivide} is ${typeof resultDivide}`);
 
 var resultAdd = resultDivide + 10;
 console.log(`Result is: ${resultAdd}`);
@@ -10,3 +10,9 @@ console.log(`Result is: ${resultMul}`);
 
 var num = new Number("Prathamesh"); 
 console.log(`num is: ${num}`);
+
+
+console.log(`-------------`);
+var resultDivide = 0/10;
+console.log(`Result is: ${resultDivide}`);
+console.log(`Type of ${resultDivide} is ${typeof resultDivide}`);
